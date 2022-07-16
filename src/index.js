429cfea7b4b280e9235b055b12bc51e260ee45db
Maria T. Andersen <mteilmann93@gmail.com>
2022-07-16 17:55:08 +0200

    implement initial communication with frontend

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const http = require('http');
 const server = http.createServer(app);
 const serverConfig = require('./server.config.json');
 const { Server } = require('socket.io');
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+  },
+});
+
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send(`<html><head><script src="/socket.io/socket.io.js"></script>
@@ -15,6 +22,12 @@ app.get('/', (req, res) => {
 
 io.on('connection', (socket) => {
   console.log('a user connected');
+
+  socket.on('chat message', (msg) => {
+    console.log(msg);
+  });
+
+  socket.emit('chat message', 'Hello from the backend!');
 });
 
 server.listen(serverConfig.port, () => {
